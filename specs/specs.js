485910ecd93a9ed340c087f4fe880483b96f7811
jordan meier and sara wagner <jbirdmeier@gmail.com>
2016-01-21 14:39:59 -0800

    Add functinality to test 1, where you can search for vowels using regular expressions

--- specs/specs.js
+++ specs/specs.js
@@ -1,17 +1,11 @@
-describe('countBy1', function() {
-  it("it will count to a given number by increments of 1", function() {
-    expect(countBy1(10,1)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+describe('findVowel', function() {
+  it("will find vowels in an entered string and return true", function() {
+    expect(findVowel("horse")).to.equal(true);
   });
-
 });
 
-
-describe('countBy4', function () {
-  it("it will count to a given number by a given increment", function() {
-    expect(countBy4(0,16,4)).to.eql([0, 4, 8, 12, 16]);
-  });
-  it("it will return false if values entered are not numbers", function () {
-    expect(countBy4("word")).to.equal(false);
-  });
-
-});
+// describe('replaceVowel', function () {
+//   it("will replace all vowels with '-' characters", function () {
+//     expect(replaceVowel("apple")).to.equal("-ppl-")
+//   });
+// });
